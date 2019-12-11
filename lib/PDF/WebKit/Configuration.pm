31ca69be04341f089c15627c85c2e84effa748bd
H.Merijn Brand <h.m.brand@xs4all.nl>
2019-12-11 15:37:27 +0100

    Default paper size should match
    "Letter" may be useful in the USA, but it is utterly annoying in EU

--- lib/PDF/WebKit/Configuration.pm
+++ lib/PDF/WebKit/Configuration.pm
@@ -11,11 +11,19 @@ has wkhtmltopdf     => ( is => 'rw', builder => '_find_wkhtmltopdf', lazy => 1 )
 around 'BUILDARGS' => sub {
   my $orig = shift;
   my $self = shift;
+  my $page_size = $ENV{LC_PAPER} || "";
+  if ($page_size =~ m/^297\b/) {
+    $page_size = "A4";
+  }
+  if (!$page_size && open my $fh, "<", "/etc/papersize") {
+    chomp ($page_size = uc <$fh>);
+    close $fh;
+  }
   return $self->$orig({
     meta_tag_prefix => 'pdf-webkit-',
     default_options => {
       disable_smart_shrinking => undef,
-      page_size => 'Letter',
+      page_size => $page_size || 'Letter',
       margin_top => '0.75in',
       margin_right => '0.75in',
       margin_bottom => '0.75in',
@@ -29,13 +37,10 @@ sub _find_wkhtmltopdf {
   my $self = shift;
   my $which = $^O eq "MSWin32" ? "where" : "which";
   my $found = `$which wkhtmltopdf`;
-  if ($? == 0) {
-    chomp($found);
-    return $found;
-  }
-  else {
-    return undef;
-  }
+  $?  and return;
+
+  chomp($found);
+  return $found;
 }
 
 my $_config;
